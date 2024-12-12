0173d536f66e218dfa74fbeb26493f2c683e1309
Arya <arya@KALI>
2024-12-12 11:43:48 +0700

    Fix error document path

---
+++ Onion/403.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>403 Forbidden</title>
+   <link rel="stylesheet" href="/public/css/403.css">
+</head>
+<body>
+    <div class="not-found">
+        <h1>403</h1>
+        <p>You do not have permission to access this page.</p>
+        <button onclick="window.history.back()">back to previous page</button>
+    </div>
+</body>
+</html>
---
+++ Onion/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+    <link rel="stylesheet" href="/public/css/404.css">
+</head>
+<body>
+    <div class="not-found">
+        <h1>404</h1>
+        <p>Halaman yang Anda cari tidak ditemukan.</p>
+        <button onclick="window.history.back()">back to previous page</button>
+    </div>
+</body>
+</html>
