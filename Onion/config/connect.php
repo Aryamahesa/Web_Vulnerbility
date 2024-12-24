934de35987379a4aafa76228d26f2022e9bc2a7b
Arya <arya@KALI>
2024-12-24 22:16:48 +0700

    Update Program

--- Onion/config/connect.php
+++ Onion/config/connect.php
@@ -1,13 +1,13 @@
 <?php
 $host = "localhost";
-$userConfig = "soc";
-$password = "soc123";
-$dbName = "socDB";
+$userConfig = "soc"; // Ganti dengan nama user di backup_db.sql
+$password = "soc123"; // Ganti dengan password user di backup_db.sql
+$dbName = "socDB"; // Nama database yang dibuat
 
 $conn = new mysqli($host, $userConfig, $password, $dbName);
 
 if ($conn -> connect_error){
     die ("Koneksi gagal : " . $conn -> connect_error);
 }
-    
+
 ?>
